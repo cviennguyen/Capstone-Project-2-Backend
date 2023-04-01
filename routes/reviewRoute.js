df872ae969b0eb3dc2ee06526ff0343c16ff1b0f
nguyencongvien <nguyencongvien13@gmail.com>
2023-03-31 20:57:31 -0400

    add some features

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -6,13 +6,9 @@ const router = express.Router();
 router
   .route("/")
   .get(reviewController.getAllReview)
-<<<<<<< HEAD
   .post(reviewController.createReview)
   .delete(reviewController.deleteReview);
 
 router.route("/:id").delete(reviewController.deleteReview);
-=======
-  .post(reviewController.createReview);
->>>>>>> 30c05cc2b48c3eefca678cceaa2abe7196dd4bb1
 
 module.exports = router;
--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -4,10 +4,6 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 const reviewController = require("../controllers/reviewController");
 const favoriteController = require("../controllers/favoriteController");
-<<<<<<< HEAD
-=======
-
->>>>>>> 30c05cc2b48c3eefca678cceaa2abe7196dd4bb1
 router
   .route("/")
   .get(authController.protect, serviceController.getAllServices)
@@ -21,15 +17,7 @@ router
     authController.restrictTo("admin", "serviceProvider"),
     serviceController.updateService
   )
-<<<<<<< HEAD
   .delete(authController.protect, serviceController.deleteService);
-=======
-  .delete(
-    authController.protect,
-    authController.restrictTo("admin", "serviceProvider"),
-    serviceController.deleteService
-  );
->>>>>>> 30c05cc2b48c3eefca678cceaa2abe7196dd4bb1
 
 router
   .route("/:serviceId/review")
